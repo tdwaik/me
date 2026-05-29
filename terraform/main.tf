ed2ffa14b2af5eb771dd0e3c3f56b6ed0060122f
Thaer Aldwaik <153673242+tald-si@users.noreply.github.com>
2026-05-29 16:46:50 +0300

    Fix deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -373,37 +373,21 @@ data "aws_iam_policy_document" "deploy" {
     resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/date-api-lambda-role"]
   }
 
-  # Read permissions Terraform needs to refresh all managed resources during plan/apply
+  # Read-only permissions Terraform needs to refresh all managed resources during plan/apply
   statement {
     sid = "AllowTerraformRefresh"
     actions = [
-      "acm:DescribeCertificate",
-      "acm:ListTagsForCertificate",
-      "cloudfront:GetDistribution",
-      "cloudfront:GetDistributionConfig",
-      "cloudfront:GetOriginAccessControl",
-      "cloudfront:GetOriginAccessControlConfig",
-      "cloudfront:DescribeFunction",
-      "cloudfront:GetFunction",
-      "cloudfront:ListTagsForResource",
-      "iam:GetOpenIDConnectProvider",
-      "iam:GetRole",
-      "iam:GetRolePolicy",
-      "iam:ListRolePolicies",
-      "iam:ListAttachedRolePolicies",
-      "iam:ListInstanceProfilesForRole",
-      "iam:GetPolicy",
-      "iam:GetPolicyVersion",
-      "iam:ListPolicyVersions",
-      "iam:ListRoleTags",
-      "logs:DescribeLogGroups",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketVersioning",
-      "s3:GetEncryptionConfiguration",
-      "s3:GetBucketOwnershipControls",
-      "s3:GetBucketPublicAccessBlock",
-      "s3:GetBucketLocation",
-      "s3:GetBucketTagging",
+      "acm:Describe*",
+      "acm:Get*",
+      "acm:List*",
+      "cloudfront:Describe*",
+      "cloudfront:Get*",
+      "cloudfront:List*",
+      "iam:Get*",
+      "iam:List*",
+      "logs:Describe*",
+      "s3:Get*",
+      "s3:List*",
     ]
     resources = ["*"]
   }
